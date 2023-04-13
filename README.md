6386bc9d0e1747acc21ed4f4a5f7b64505e1a6f0
sfu-dataminers <dataminers.sfu@gmail.com>
2023-04-12 21:07:46 -0700

    adding remaining files

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# 713nlp
+## CMPT 713 Project
+# Polyglot Refinement: Neural Machine Translation for Low Resource Languages
+
+In a one-to-many low-resource neural machine translation (NMT) setting, we wish to demonstrate the impressive utility of multi-parallel corpora for transfer learning. We present results from a systematic evaluation of multistage fine-tuning configurations, which include pre-training on a large parallel external corpus for both the English language and a supporting target language i.e. Asian languages. Our work is a recreation of(Dabreet al., 2019). We extend their work by using state-of-art tokenizers to tokenize Asian language sentences. In order to demonstrate the true power of multilingualism, we are attempting to show that multi-parallel corpora are incredibly useful despite their rarity and content related redundancy. We observed that this multistage fine-tuning can result in 4–8 BLEU score gains over a straightforward one–to–one model, even when the helping target language is not one of the target languages of our concern.
+
+## How To Run
+
+For training the model and analysing them, *polyglot_nmt.ipynb* notebook was used.
+
+In order to train the model on a specific target language change the *yylang/ target_lang* parametre from the notebook. If you wish to train on differen helper language (chinese(zh)/ japanese(ja)) change the *xxlang* parametre.
+
+To test the BLEU score of the model run the comman below
+```
+python check_bleu.py --model <model-name> --targetlang <target-language>
+```
+For Example,
+
+```
+python check_bleu.py --model NMT_PFT_en-zh-to-ms --targetlang ms
+```
+
+The above code will produce the resuls below: 
+
+![terminal](images/terminal_op.png)
+*Figure 01:Output of the polyglot_check.py*
+
+
+The models can be found [here](https://drive.google.com/drive/folders/1IJw_hmjMfxxPXdxaLrTtI4WGuyECy4Ee?usp=sharing).
+
+## Dataset
+
+### [ALT](https://www2.nict.go.jp/astrec-att/member/mutiyama/ALT/)
+We used the ALT multilingual multi-parallel corpus (Riza et al., 2016) as our test corpora because it provides identical test sentences in various languages, enabling us to establish whether multilingualism is actually the source of better translation quality. As the Asian target languages (i.e., yy) we have considered the following languages for our analysis: Filipino (fi), Hindi (hi), Indonesian (id), Burmese (ms), Malay (my), Thai (th), and Vietnamese (vi) from the family of Asian languages. This dataset is tokenized using a mbart50-like tokenizer. We randomly divided the ALT data into 18000 train, 1000 validation and 1000 test multi-parallel sentences for training, development, and testing.
+
+### [Opus100](https://opus.nlpl.eu/opus-100.php) 
+We used two parallel corpora with two different target languages for the resource-rich en-xx data. One such corpus is the English-Chinese corpus (Opus En-Zh), which contains 209,491 parallel sentences. We chose it to see if the target language (Chinese) other than the target languages in our context is useful. Separately, we experimented with the English-Japanese corpus (Opus En-Ja), which contains 440,288 parallel sentences of Japanese-to-English translation.
+
+
+## Training 
+
+We first fine-tuned a pre-trained model on a parallel corpus for the resource-rich language pair single pair en-xx([Huggingface-en-zh](https://huggingface.co/Helsinki-NLP/opus-mt-en-zh)). As a next step we finetuned the pre-trained model on a parallel dataset of the Asian languages. Later this resultant mode is finetuned further on the single target language.
+
+![Approach](images/fig_approach.png)
+*Figure 01: Training Approach used in the project.*
+
+## Results
+
+![Scores](images/results_table.png)
+*Figure 02:BLEU Scores of the models*
+
+![Comparison1](images/zh_bleu_label.png)
+*Figure 03: Base Language Chinese*
+
+![Comparison2](images/ja_bleu_label.png)
+*Figure 04: Base Language Japanese*
+
+
--- make_tokens.py
+++ make_tokens.py
@@ -1,74 +1,47 @@
+# -*- coding: utf-8 -*-
+from typing import List
 from tqdm.auto import tqdm
 from transformers import MarianTokenizer, MBart50TokenizerFast, utils
 from datasets import Dataset, load_dataset
 utils.logging.set_verbosity(50)
 
-
-def update_tokenizer(
-    teacher,
-    student,
-    sentences,
-    CREATE_TOKENS_FILE=False,
-    TOKENS_FILE_NAME='Tokens.txt'
-):
-    """Updated the marian tokenizer with newly generated tokens from mbart model.
-
-    Parameters
-    ----------
-    teacher : str
-        The name of mbart model to tokenize sentences
-        valied model for MBart50TokenizerFast
-
-    student : str
-        The name of marian model to be updated
-        valied model for MarianTokenizer
-
-    senetences: List
-        The list of sentences to be tokenized by teacher
-
-    CREATE_TOKENS_FILE : bool, Optional, default to False
-        Indicate if the text file containing newly added tokens is to be generated.
-
-    TOKENS_FILE_NAME: , Optional, Default to 'Tokens.txt'
-        path for Tokens file.
-
-
-    Returns
-    ------
-    Updated MarianTokenizer tokenizer
+def make_tokens(asian_tokenizer: str, base_tokenizer: str, sent: List[str], create_tokens_file: bool = True, tokens_file_name: str = r'data/tokens.txt' ) -> MarianTokenizer:
     """
+    Append the Asian language tokens generated using MBART model to the base tokenizer.
+    
+    :param asian_tokenizer (str): Tokenizer to create the tokens for the asian languages.
+    :param base_tokenizer (str): Tokenizer to append the asian language tokens.
+    :param sent (list): input to tokenize
+    :param create_tokens_file (Bool): Boolean to make the token file or not.
+    :param tokens_file_name (str): Name of the token file.
+    :return: Tokenizer object.
+    """
+    
+    base_t = MarianTokenizer.from_pretrained(base_tokenizer)
+    asian_t = MBart50TokenizerFast.from_pretrained(asian_tokenizer)
 
-    marian_tokenizer = MarianTokenizer.from_pretrained(student)
-    mbart_tokenizer = MBart50TokenizerFast.from_pretrained(teacher)
+    tokens_ls = list()
 
-    sents = sentences
-
-    tokensZoo = []
-
-    for s in tqdm(sents):
+    for s in tqdm(sent):
         try:
-            tokens = mbart_tokenizer.tokenize(s)
+            tokens = asian_t.tokenize(s)
             tokens = [a.lstrip("▁") for a in tokens]
-            tokensZoo.extend(tokens)
-        except:
-            print(s, type(s), tokens)
+            tokens_ls.extend(tokens)
+        except Exception as e:
+            print(f"Error In Tokenization: {e}")
+            
+    unique_tokens = list(dict.fromkeys(tokens_ls))
+    if "" in unique_tokens:
+        unique_tokens.remove("")
 
-    # discard the duplicates but keep the order
-    unique_tokens_zoo = list(dict.fromkeys(tokensZoo))
-    if "" in unique_tokens_zoo:
-        unique_tokens_zoo.remove("")
+    base_t.add_tokens(unique_tokens, special_tokens=True)
 
-    print(f"{len(unique_tokens_zoo)} tokens to be added.")
-    print(f"initial vocab size: {len(marian_tokenizer)}")
-    marian_tokenizer.add_tokens(list(unique_tokens_zoo), special_tokens=True)
-    print(f"final vocab size: {len(marian_tokenizer)}")
-
-    if CREATE_TOKENS_FILE:
-        with open(TOKENS_FILE_NAME, "w") as outfile:
+    if create_tokens_file:
+        with open(tokens_file_name, "w") as outfile:
             outfile.write(
-                "\n".join(list(marian_tokenizer.added_tokens_encoder.keys())))
+                "\n".join(list(base_t.added_tokens_encoder.keys())))
 
-    return marian_tokenizer
+    return base_t
 
 
 if __name__ == '__main__':
@@ -76,7 +49,4 @@ if __name__ == '__main__':
     teacher = "facebook/mbart-large-50-one-to-many-mmt"
     student = 'Helsinki-NLP/opus-mt-en-zh'
     sents = alt_ds["train"]["Sent_yy"]
-    marian_tokenizer = update_tokenizer(student=student,
-                                        teacher=teacher,
-                                        sentences=sents,
-                                        CREATE_TOKENS_FILE=True)
+    marian_tokenizer = make_tokens(asian_tokenizer=teacher, base_tokenizer=student, sent=sents)
